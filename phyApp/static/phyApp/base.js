e0c30c11317c9535d66e60d0c198967e1cf888e2
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2021-10-09 22:24:17 +0530

    css And Js FIles added

---
+++ phyApp/static/phyApp/base.js
@@ -0,0 +1,16 @@
+
+function navFunctionOpen(){
+
+    $('.nav').css("transform" , "translateX(0%)");
+    $('.main').css("transform" , "translateX(15%)")
+}
+
+function navFunctionClose(){
+
+    $('.nav').css("transform" , "translateX(-100%)")
+    $('.main').css("transform" , "translateX(0%)")
+}
+
+
+
+
---
+++ phyApp/static/phyApp/simulations.js
@@ -0,0 +1,4 @@
+
+function myHref(web){
+    window.location.href = web 
+}
