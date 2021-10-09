a241f088e10ae18347ce0f332ae381d019b1345a
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2021-10-09 21:36:48 +0530

    edited admin.py

--- phyApp/admin.py
+++ phyApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Simulation_Project , Video_Project
 
 # Register your models here.
+
+admin.site.register(Simulation_Project),
+admin.site.register(Video_Project)
