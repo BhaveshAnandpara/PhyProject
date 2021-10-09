174ddd9c21fdd618efb037857545b05ce20c13dc
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2021-10-09 21:34:55 +0530

    Added Modules

--- phyApp/models.py
+++ phyApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Simulation_Project(models.Model):
+    project_id = models.AutoField
+    project_name = models.CharField(max_length=200)
+    author_name = models.CharField(max_length=200)
+    geogebra_link = models.CharField(max_length=1000)
+    youtube_link = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='images' , null=True)
+
+    def __str__(self):
+        return self.project_name
+
+class Video_Project(models.Model):
+    project_id = models.AutoField
+    project_name = models.CharField(max_length=200)
+    author_name = models.CharField(max_length=200)
+    youtube_link = models.CharField(max_length=1000)
+
+
+    def __str__(self):
+        return self.project_name
