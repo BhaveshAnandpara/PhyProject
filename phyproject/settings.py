bc94abfa83859a4360416472eac226e24077b238
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2021-10-09 21:23:38 +0530

    Static roots in settings

--- phyproject/settings.py
+++ phyproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import django_heroku
 
@@ -131,3 +132,5 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
